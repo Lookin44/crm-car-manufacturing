1ffe02d954b1d6d160e81a800f3560928167754e
Егор Лукин <look-in44@yandex.ru>
2023-04-06 15:28:41 +0300

    Add function position.

--- back/telegram_bot/main_bot.py
+++ back/telegram_bot/main_bot.py
@@ -1,6 +1,5 @@
 import logging
 import os
-from typing import Dict
 
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
@@ -12,8 +11,8 @@ from telegram.ext import (
     filters,
 )
 
-from asgiref.sync import sync_to_async
 from api.models import *
+from .utils import facts_to_str, check_time
 
 
 logging.basicConfig(
@@ -31,14 +30,14 @@ reply_keyboard = [
     ["Отчество", "Смена"],
     ["Цех", "Участок"],
     ["Табельный номер"],
+    ["Должность"],
     ["Готово"],
 ]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-
-
-def facts_to_str(user_data: Dict[str, str]) -> str:
-    facts = [f"{key} - {value}" for key, value in user_data.items()]
-    return "\n".join(facts).join(["\n", "\n"])
+markup = ReplyKeyboardMarkup(
+    reply_keyboard,
+    one_time_keyboard=True,
+    input_field_placeholder="Не вводить данные!"
+)
 
 
 async def start(
@@ -52,14 +51,14 @@ async def start(
     ).aexists():
         user = await CustomUser.objects.aget(telegram_id=user_telegram_id)
         await update.message.reply_text(
-            f"Добро пожаловать обратно, {user.first_name} "
+            f"{check_time()}, {user.first_name} "
             f"{user.patronymic}!"
         )
     else:
         await update.message.reply_text(
-            f"Здравствуйте, {telegram_user.first_name} "
+            f"{check_time()}, {telegram_user.first_name} "
             f"{telegram_user.last_name}! "
-            f"Пройдите регистрацию."
+            f"Пройдите регистрацию. /registration"
         )
 
 
@@ -75,70 +74,135 @@ async def registration(
     return CHOOSING
 
 
+async def handle_name(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    await update.message.reply_text("Введите Ваше имя:")
+
+
+async def handle_surname(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    await update.message.reply_text("Введите Вашу фамилию:")
+
+
+async def handle_patronymic(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    await update.message.reply_text("Введите Ваше отчество:")
+
+
+async def handle_employee_id(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    await update.message.reply_text("Введите Ваш табельный номер:")
+
+
+async def handle_shift(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    keyboard_shifts = [
+        [shift.letter_designation async for shift in Shift.objects.all()]
+    ]
+    markup_shifts = ReplyKeyboardMarkup(
+        keyboard_shifts,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="Не вводить данные!"
+    )
+    await update.message.reply_text(
+        "Укажите Вашу смену:",
+        reply_markup=markup_shifts
+    )
+
+
+async def handle_shop(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    keyboard_shops = [[shop.name] async for shop in Shop.objects.all()]
+    markup_shifts = ReplyKeyboardMarkup(
+        keyboard_shops,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="Не вводить данные!"
+    )
+    await update.message.reply_text(
+        "Укажите Ваш цех:",
+        reply_markup=markup_shifts
+    )
+
+
+async def handle_zone(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    shop_name = context.user_data['Цех']
+    keyboard_zones = [
+        [zone.name] async for zone in Zone.objects.filter(
+            shop__name=shop_name
+        )
+    ]
+    markup_zones = ReplyKeyboardMarkup(
+        keyboard_zones,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="Не вводить данные!"
+    )
+    await update.message.reply_text(
+        "Укажите Ваш участок:",
+        reply_markup=markup_zones
+    )
+
+
+async def handle_position(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    markup_shifts = ReplyKeyboardMarkup(
+        [[pos.name] async for pos in Position.objects.all()],
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="Не вводить данные!"
+    )
+    await update.message.reply_text(
+        "Укажите Вашу должность:",
+        reply_markup=markup_shifts
+    )
+
+
 async def regular_choice(
         update: Update,
         context: ContextTypes.DEFAULT_TYPE
 ) -> int:
 
+    switcher = {
+        "Имя": handle_name,
+        "Фамилия": handle_surname,
+        "Отчество": handle_patronymic,
+        "Смена": handle_shift,
+        "Цех": handle_shop,
+        "Участок": handle_zone,
+        "Табельный номер": handle_employee_id,
+        "Должность": handle_position,
+    }
+
     text = update.message.text
     context.user_data["choice"] = text
-    if text == "Имя":
-        await update.message.reply_text("Введите Ваше имя:")
-    elif text == "Фамилия":
-        await update.message.reply_text("Введите Вашу фамилию:")
-    elif text == "Отчество":
-        await update.message.reply_text("Введите Ваше отчество:")
-    elif text == "Смена":
-        keyboard_shifts = [
-            [shift.letter_designation async for shift in Shift.objects.all()]
-        ]
-        markup_shifts = ReplyKeyboardMarkup(
-            keyboard_shifts,
-            resize_keyboard=True,
-            one_time_keyboard=True,
-            input_field_placeholder="Не вводить данные!"
-        )
-        await update.message.reply_text(
-            "Укажите Вашу смену:",
-            reply_markup=markup_shifts
-        )
-    elif text == "Цех":
-        keyboard_shops = [[shop.name] async for shop in Shop.objects.all()]
-        markup_shifts = ReplyKeyboardMarkup(
-            keyboard_shops,
-            resize_keyboard=True,
-            one_time_keyboard=True,
-            input_field_placeholder="Не вводить данные!"
-        )
-        await update.message.reply_text(
-            "Укажите Ваш цех:",
-            reply_markup=markup_shifts
-        )
-    elif text == "Участок":
-        shop_name = context.user_data['Цех']
-        keyboard_zones = [
-            [zone.name] async for zone in Zone.objects.filter(
-                shop__name=shop_name
-            )
-        ]
-        markup_zones = ReplyKeyboardMarkup(
-            keyboard_zones,
-            resize_keyboard=True,
-            one_time_keyboard=True,
-            input_field_placeholder="Не вводить данные!"
-        )
-        await update.message.reply_text(
-            "Укажите Ваш участок:",
-            reply_markup=markup_zones
-        )
-    elif text == "Табельный номер":
-        await update.message.reply_text("Введите Ваш табельный номер:")
 
-    # TODO: Необходимо добавить вариант с выбором грейда и должности,
-    #  так же по хорошему необходимо разделить эти функции в разные файлы,
-    #  клавиатуры в один файл а может и в директорию,  функции в другие
-
-    return TYPING_REPLY
+    if text in switcher:
+        await switcher[text](update, context)
+        return TYPING_REPLY
+    else:
+        await update.message.reply_text(
+            "Я не понимаю, о чем Вы говорите. Попробуйте еще раз."
+        )
+        return CHOOSING
 
 
 async def received_information(
@@ -198,14 +262,17 @@ def main() -> None:
             CHOOSING: [
                 MessageHandler(
                     filters.Regex(
-                        "^(Имя|Фамилия|Отчество|Смена|Цех|Участок|Табельный номер)$"
+                        "^(Имя|Фамилия|Отчество|Смена|Цех|"
+                        "Участок|Табельный номер|Должность)$"
                     ),
                     regular_choice,
                 )
             ],
             TYPING_REPLY: [
                 MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Готово$")),
+                    filters.TEXT & ~(
+                            filters.COMMAND | filters.Regex("^Готово$")
+                    ),
                     received_information,
                 )
             ],
