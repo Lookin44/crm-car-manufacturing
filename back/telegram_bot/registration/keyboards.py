3ea94c58976e51698a384596a5fc41de3d59c5cc
Egor <look-in44@yandex.ru>
2023-05-08 08:50:59 +0300

    Refactoring functions at conversation handler of registration

--- back/telegram_bot/registration/keyboards.py
+++ back/telegram_bot/registration/keyboards.py
@@ -1,13 +1,15 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes
 
 from api.models import *
+from telegram_bot.state_list import *
 
 
 async def choice_shift_keyboard():
     button = [
         [InlineKeyboardButton(
             text=shift.letter_designation,
-            callback_data=shift.id
+            callback_data=shift.letter_designation
         )] async for shift in Shift.objects.all()
     ]
     return InlineKeyboardMarkup(button)
@@ -17,18 +19,18 @@ async def choice_shop_keyboard():
     button = [
         [InlineKeyboardButton(
             text=shop.name,
-            callback_data=shop.id
+            callback_data=shop.name
         )] async for shop in Shop.objects.all()
     ]
     return InlineKeyboardMarkup(button)
 
 
-async def choice_zone_keyboard(shop_id):
+async def choice_zone_keyboard(shop_name):
     button = [
         [InlineKeyboardButton(
             text=zone.name,
-            callback_data=zone.id
-        )] async for zone in Zone.objects.filter(shop_id=shop_id)
+            callback_data=zone.name
+        )] async for zone in Zone.objects.filter(shop__name=shop_name)
     ]
     return InlineKeyboardMarkup(button)
 
@@ -37,7 +39,34 @@ async def choice_position_keyboard():
     button = [
         [InlineKeyboardButton(
             text=pos.name,
-            callback_data=pos.id
+            callback_data=pos.name
         )] async for pos in Position.objects.all()
+    ]
+    return InlineKeyboardMarkup(button)
+
+
+async def data_keyboard(context: ContextTypes.DEFAULT_TYPE):
+    user_data = context.user_data
+    user_name = user_data.get(NAME)
+    user_last_name = user_data.get(LAST_NAME)
+    user_patronymic = user_data.get(PATRONYMIC)
+    user_employee_id = user_data.get(EMPLOYEE_ID)
+    user_shift = user_data.get(SHIFT)
+    user_position = user_data.get(POSITION)
+    user_shop = user_data.get(SHOP)
+    user_zone = user_data.get(ZONE)
+    button = [
+        [
+            InlineKeyboardButton(text=user_name, callback_data=NAME),
+            InlineKeyboardButton(text=user_last_name, callback_data=LAST_NAME),
+        ],
+        [InlineKeyboardButton(text=user_patronymic, callback_data=PATRONYMIC),],
+        [InlineKeyboardButton(text=user_employee_id, callback_data=EMPLOYEE_ID),],
+        [InlineKeyboardButton(text=user_position, callback_data=POSITION),],
+        [
+            InlineKeyboardButton(text=user_shift, callback_data=SHIFT),
+            InlineKeyboardButton(text=user_shop, callback_data=SHOP),
+            InlineKeyboardButton(text=user_zone, callback_data=ZONE),
+        ],
     ]
     return InlineKeyboardMarkup(button)
--- back/telegram_bot/registration/registration_handler.py
+++ back/telegram_bot/registration/registration_handler.py
@@ -19,20 +19,61 @@ from .keyboards import (
     choice_shop_keyboard,
     choice_zone_keyboard,
     choice_position_keyboard,
+    data_keyboard
 )
 from telegram_bot.state_list import *
 
 
 text_launch = {
-    NAME: {'text': 'Укажите Вашу фамилию:', 'exit_point': LAST_NAME},
-    LAST_NAME: {'text': 'Укажите Ваше отчество:', 'exit_point': PATRONYMIC},
-    PATRONYMIC: {'text': 'Укажите Ваш табельный номер:', 'exit_point': EMPLOYEE_ID},
-    EMPLOYEE_ID: {'text': 'Сделайте свое селфи', 'exit_point': PHOTO},
-    PHOTO: {'text': 'Укажите Вашу смену:', 'exit_point': SHIFT},
-    SHIFT: {'text': 'Укажите Вашу должность:', 'exit_point': POSITION},
-    POSITION: {'text': 'Укажите Ваш цех:', 'exit_point': SHOP},
-    SHOP: {'text': 'Укажите Ваш участок', 'exit_point': ZONE},
-    ZONE: {'text': 'Верны ли следующие данные:', 'exit_point': CHOOSE_EDIT_INFO},
+    NAME:
+        {
+            'text': 'Укажите Вашу фамилию:',
+            'exit_point': LAST_NAME,
+        },
+    LAST_NAME:
+        {
+            'text': 'Укажите Ваше отчество:',
+            'exit_point': PATRONYMIC,
+        },
+    PATRONYMIC:
+        {
+            'text': 'Укажите Ваш табельный номер:',
+            'exit_point': EMPLOYEE_ID,
+        },
+    EMPLOYEE_ID:
+        {
+            'text': 'Сделайте свое селфи',
+            'exit_point': PHOTO,
+        },
+    PHOTO:
+        {
+            'text': 'Укажите Вашу смену:',
+            'exit_point': SHIFT,
+        },
+    SHIFT:
+        {
+            'text': 'Укажите Вашу должность:',
+            'exit_point': POSITION,
+            'keyboard': choice_position_keyboard,
+        },
+    POSITION:
+        {
+            'text': 'Укажите Ваш цех:',
+            'exit_point': SHOP,
+            'keyboard': choice_shop_keyboard,
+        },
+    SHOP:
+        {
+            'text': 'Укажите Ваш участок',
+            'exit_point': ZONE,
+            'keyboard': choice_zone_keyboard,
+        },
+    ZONE:
+        {
+            'text': 'Верны ли следующие данные:',
+            'exit_point': CHOOSE_EDIT_INFO,
+            'keyboard': data_keyboard,
+        },
 }
 
 
@@ -75,74 +116,32 @@ async def photo_save(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return exit_point
 
 
-async def shift_choose(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def choose_answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_data = context.user_data
     current_state = user_data.get('current_state')
     message_text = text_launch.get(current_state).get('text')
     exit_point = text_launch.get(current_state).get('exit_point')
+    keyboard = text_launch.get(current_state).get('keyboard')
 
     user_data[current_state] = update.callback_query.data
 
     await update.callback_query.answer()
-    await update.callback_query.edit_message_text(
-        message_text,
-        reply_markup=await choice_position_keyboard()
-    )
+    if current_state == SHOP:
+        await update.callback_query.edit_message_text(
+            message_text,
+            reply_markup=await keyboard(user_data.get(current_state))
+        )
+    elif current_state == ZONE:
+        await update.callback_query.edit_message_text(
+            message_text,
+            reply_markup=keyboard(context)
+        )
+    else:
+        await update.callback_query.edit_message_text(
+            message_text,
+            reply_markup=await keyboard()
+        )
     user_data['current_state'] = exit_point
-    return exit_point
-
-
-async def position_choose(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_data = context.user_data
-    current_state = user_data.get('current_state')
-    message_text = text_launch.get(current_state).get('text')
-    exit_point = text_launch.get(current_state).get('exit_point')
-
-    user_data[current_state] = update.callback_query.data
-
-    await update.callback_query.answer()
-    await update.callback_query.edit_message_text(
-        message_text,
-        reply_markup=await choice_shop_keyboard()
-    )
-    user_data['current_state'] = exit_point
-    return exit_point
-
-
-async def shop_choose(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_data = context.user_data
-    current_state = user_data.get('current_state')
-    message_text = text_launch.get(current_state).get('text')
-    exit_point = text_launch.get(current_state).get('exit_point')
-
-    user_data[current_state] = update.callback_query.data
-
-    await update.callback_query.answer()
-    await update.callback_query.edit_message_text(
-        message_text,
-        reply_markup=await choice_zone_keyboard(user_data.get(current_state))
-    )
-    user_data['current_state'] = exit_point
-
-    return exit_point
-
-
-async def zone_choose(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_data = context.user_data
-    current_state = user_data.get('current_state')
-    message_text = text_launch.get(current_state).get('text')
-    exit_point = text_launch.get(current_state).get('exit_point')
-
-    user_data[current_state] = update.callback_query.data
-
-    await update.callback_query.answer()
-    await update.callback_query.edit_message_text(
-        message_text,
-        reply_markup=await choice_zone_keyboard(user_data.get(current_state))
-    )
-    user_data['current_state'] = exit_point
-
-    print(user_data)
     return exit_point
 
 
@@ -165,10 +164,10 @@ registration_handler = ConversationHandler(
         EMPLOYEE_ID:
             [MessageHandler(filters.TEXT & ~filters.COMMAND, typing_answer)],
         PHOTO: [MessageHandler(filters.PHOTO & ~filters.COMMAND, photo_save)],
-        SHIFT: [CallbackQueryHandler(shift_choose)],
-        POSITION: [CallbackQueryHandler(position_choose)],
-        SHOP: [CallbackQueryHandler(shop_choose)],
-        ZONE: [CallbackQueryHandler(zone_choose)],
+        SHIFT: [CallbackQueryHandler(choose_answer)],
+        POSITION: [CallbackQueryHandler(choose_answer)],
+        SHOP: [CallbackQueryHandler(choose_answer)],
+        ZONE: [CallbackQueryHandler(choose_answer)],
     },
     fallbacks=[CallbackQueryHandler(done, pattern="^" + str(END) + "$")],
     map_to_parent={
